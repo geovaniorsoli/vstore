91248431d2dd22e0958b097dd77113e2fe0c7886
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-07-23 18:21:47 -0300

    Update elo.tsx

--- app/array/elo.tsx
+++ app/array/elo.tsx
@@ -67,7 +67,7 @@ export const elosWish: Elo[] = [
     { name: "Imortal I", value: 21, price: imortal, src: "rank/Immortal_1_Rank.png" },
     { name: "Imortal II", value: 22,price:  imortal, src: "rank/Immortal_2_Rank.png" },
     { name: "Imortal III", value: 23, price: imortal, src: "rank/Immortal_3_Rank.png" },
-    { name: "Radiant", value: 24, price: 200, src: "rank/Radiant_Rank.png" },
+    { name: "Radiant", value: 24, price: 175, src: "rank/Radiant_Rank.png" },
 ];
 
 
